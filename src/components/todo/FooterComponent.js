5804e6e97d5ac0f91705fb0e76156b96422c9380
Yiwen789 <luoy2@Carleton.edu>
2021-08-30 09:33:37 -0400

    added basic components

---
+++ src/components/todo/FooterComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <span className="text-muted">Thank you for using! For more information please visit my <a href="https://github.com/Yiwen789">GitHub</a></span>
+            </footer>
+        )
+    }
+}
+
+export default FooterComponent
---
+++ src/components/todo/TodoApp.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import AuthenticatedRoute from './AuthenticatedRoute';
+import LoginComponent from './LoginComponent';
+import ListTodosComponent from './ListTodosComponent';
+import ErrorComponent from './ErrorComponent';
+import HeaderComponent from './HeaderComponent';
+import FooterComponent from './FooterComponent';
+import LogoutComponent from './LogoutComponent';
+import WelcomeComponent from './WelcomeComponent';
+
+class TodoApp extends Component {
+    render() {
+        return (
+            <div className="TodoApp">
+                <Router>
+                    <>
+                        <HeaderComponent/>
+                        <Switch>
+                            <Route path="/" exact component={LoginComponent}/>
+                            <Route path="/login" exact component={LoginComponent}/>
+                            <AuthenticatedRoute path="/welcome/:name" exact component={WelcomeComponent}/>
+                            <AuthenticatedRoute path="/todos" exact component={ListTodosComponent}/>
+                            <AuthenticatedRoute path="/logout" exact component={LogoutComponent}/>
+                            <Route component={ErrorComponent}/>
+                        </Switch>
+                        <FooterComponent/>
+                    </>
+                </Router>
+                {/*<LoginComponent/>
+                <WelcomeComponent/>*/}
+            </div>
+        )
+    }
+}
+
+export default TodoApp
